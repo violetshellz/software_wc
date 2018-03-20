c23e502df923d27ace86d5d898e1bd05b3fd6cde
violetshellz <18873023176@qq.com>
2018-03-20 20:40:42 +0800

    new file:   BIN/ceshi.py 	new file:   BIN/char.c 	modified:   BIN/outputFile.txt 	modified:   BIN/result.txt 	new file:   BIN/stoptest.c 	modified:   BIN/wc.exe 	new file:   BIN/whitespace.c 	new file:   BIN/wordtest.c

---
+++ BIN/ceshi.py
@@ -0,0 +1,16 @@
+import os
+#统计字符
+#os.system(r'wc.exe -c char.c')
+#os.system(r'wc.exe -c whitespace.c')
+
+#统计单词和行数
+#os.system(r'wc.exe –w –l wordtest.c')
+
+#注释、代码、空格行数
+#os.system(r'wc.exe –a file.c')
+
+#停用词表
+#os.system('wc.exe –w stoptest.c –e stoplist.txt')
+
+#文件夹遍历
+#os.system('wc.exe -s -a –w a')
